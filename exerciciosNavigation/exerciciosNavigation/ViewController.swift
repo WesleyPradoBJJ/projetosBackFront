14c86098a8338f5a3546b26ed13aa6067cb1a664
Wesley Prado <wesleypradobjj@gmail.com>
2022-10-07 16:01:39 +0400

    Novas modificações

--- exerciciosNavigation/exerciciosNavigation/ViewController.swift
+++ exerciciosNavigation/exerciciosNavigation/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var goScreen02: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func goScreen02(_ sender: Any) {
+        performSegue(withIdentifier: "Tela02VC", sender: nil)
+    }
     
 }
 
