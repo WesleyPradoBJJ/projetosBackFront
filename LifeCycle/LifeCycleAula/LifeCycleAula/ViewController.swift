2840ce959fda77ef5a773e7741ab304c8b2c024e
Wesley Prado <wesleypradobjj@gmail.com>
2022-10-12 04:46:29 +0400

    funcoes de life cycle inseridas

--- LifeCycle/LifeCycleAula/LifeCycleAula/ViewController.swift
+++ LifeCycle/LifeCycleAula/LifeCycleAula/ViewController.swift
@@ -13,22 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print(#function)
     }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        print(#function)
-//    }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        print(#function)
-//    }
-//
-//    override func viewDidDisappear(_ animated: Bool) {
-//        print(#function)
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        print(#function)
-//    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
     
     
     @IBAction func Screen02VCButton(_ sender: UIButton) {
