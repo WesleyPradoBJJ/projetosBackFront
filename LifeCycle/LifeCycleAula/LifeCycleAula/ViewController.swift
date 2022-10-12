642490e4669b58b3fa9052709429c09197db5c09
Wesley Prado <wesleypradobjj@gmail.com>
2022-10-12 04:06:42 +0400

    First Commit criei tela para testar as funcoes do life cycle.

---
+++ LifeCycle/LifeCycleAula/LifeCycleAula/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  LifeCycleAula
+//
+//  Created by Wesley Prado on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        print(#function)
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        print(#function)
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        print(#function)
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        print(#function)
+//    }
+    
+    
+    @IBAction func Screen02VCButton(_ sender: UIButton) {
+        let vc = UIStoryboard (name: "Screen02VC", bundle:
+        nil).instantiateViewController (withIdentifier: "Screen02VC") as? Screen02VC
+        
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
+    
+}
+
