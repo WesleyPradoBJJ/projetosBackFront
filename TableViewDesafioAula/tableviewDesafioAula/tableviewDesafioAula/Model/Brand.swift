dfa91398748082c65722492a9c12ded42fa229b2
Wesley Prado <wesleypradobjj@gmail.com>
2022-10-28 04:38:57 +0400

    Desafio tableView simples

---
+++ TableViewDesafioAula/tableviewDesafioAula/tableviewDesafioAula/Model/Brand.swift
@@ -0,0 +1,13 @@
+//
+//  Brand.swift
+//  tableviewDesafioAula
+//
+//  Created by Wesley Prado on 28/10/2022.
+//
+
+import Foundation
+import UIKit
+struct Brand {
+    var brand: String
+    var image: UIImage
+}
--- TableViewDesafioAula/tableviewDesafioAula/tableviewDesafioAula/ViewController.swift
+++ TableViewDesafioAula/tableviewDesafioAula/tableviewDesafioAula/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data: [Brand] = [
+    Brand(brand: "AUDI", image: UIImage(named: "audi") ?? UIImage()),
+    Brand(brand: "BENTLEY", image: UIImage(named: "bentley") ?? UIImage()),
+    Brand(brand: "JAGUAR", image: UIImage(named: "jaguar") ?? UIImage()),
+    Brand(brand: "MUSTANG", image: UIImage(named: "mustang") ?? UIImage()),
+    Brand(brand: "OPEL", image: UIImage(named: "opel") ?? UIImage())
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configTableView()
     }
 
+    func configTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Identificador
+        tableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.identifier)
+    }
 
 }
 
+//MARK: TableView Protocols
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as? CustomTableViewCell
+        cell?.setupCell(data: data[indexPath.row])
+        return cell ?? UITableViewCell()
+    }
+    
+    // Seta o tamanho das imagens na row! 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(data[indexPath.row].brand)
+    }
+}
