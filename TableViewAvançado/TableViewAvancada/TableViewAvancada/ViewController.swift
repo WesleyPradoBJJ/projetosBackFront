c21bc00d8e2ba744fad5197e1563ee6b570fbe03
Wesley Prado <wesleypradobjj@gmail.com>
2022-11-08 02:58:00 +0400

    Criando elementos

--- TableViewAvançado/TableViewAvancada/TableViewAvancada/ViewController.swift
+++ TableViewAvançado/TableViewAvancada/TableViewAvancada/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+
+    var data: [String] = ["Caio", "Felipe", "Bruno"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
     func configTableView(){
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(NameTableViewCell.nib(), forCellReuseIdentifier: NameTableViewCell.identifier)
     }
 
 }
@@ -27,11 +30,17 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: NameTableViewCell.identifier, for: indexPath) as? NameTableViewCell
+        cell?.setupCell(name: data[indexPath.row])
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
 }
 
