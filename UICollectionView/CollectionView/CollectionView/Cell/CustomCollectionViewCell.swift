2d747072e6fc64fd0ab7c98d7b2b07fbbaf95887
Wesley Prado <wesleypradobjj@gmail.com>
2022-11-08 05:13:57 +0400

    Criando app UICollectionView

---
+++ UICollectionView/CollectionView/CollectionView/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Wesley Prado on 08/11/2022.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carImageView: UIImageView!
+    
+    
+    static let identifier: String = "CustomCollectionViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
+    //Ciclo de vida da cell, podendo comparar com o viewDidLoad da VC
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        carImageView.contentMode = .scaleAspectFill
+    }
+    
+    func setupCell(nameImage: String){
+        carImageView.image = UIImage(named: nameImage)
+    }
+
+}
---
+++ UICollectionView/CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Wesley Prado on 08/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var data: [String] = ["car1", "car2", "car3", "car4", "car5", "car6"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configCollectionView()
+    }
+    
+    func configCollectionView(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CustomCollectionViewCell.nib(), forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+            layout.estimatedItemSize = .zero
+        }
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    //número de itens por section
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    
+    //criação das células
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as? CustomCollectionViewCell
+        cell?.setupCell(nameImage: data[indexPath.row])
+        return cell ?? UICollectionViewCell()
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    //Indicar a altura/largura da celula
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 300)
+    }
+}
